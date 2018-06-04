98001d57c5209cd896c4f0c376deb242cc82ae9c
Grace Shih <vocalise@gmail.com>
2018-06-04 03:28:12 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,8 +13,9 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "SELECT COUNT()
+  "SELECT COUNT(name) FROM students WHERE tardies > 0;"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM students WHERE grade = 9"
 end
